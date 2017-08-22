eaac639778b2ea97063018bca858997495720e17
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-08-22 20:24:03 +0300

    bump to version 0.9.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if 'NO_PILLOW' not in os.environ:
 
 setup(
     name='ModernGL.ext.examples',
-    version='0.1.0',
+    version='0.9.0',
     author='Szabolcs Dombi',
     author_email='cprogrammer1994@gmail.com',
     description='ModernGL extension',
